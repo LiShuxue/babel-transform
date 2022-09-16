38464fde374bcd10fc8a644cfabbe633ada5a54c
LiShuxue <1149926505@qq.com>
2022-09-16 14:02:18 +0800

    遍历AST语法树

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 
 const fs = require('fs')
 const babel = require('@babel/core');
-const esFilePath = '/Users/lishuxue/Documents/study/babel-transform/typescript.ts';
+const traverse = require('@babel/traverse').default;
+const esFilePath = '/Users/lishuxue/Documents/study/babel-transform/es6.js';
 const esCode = fs.readFileSync(esFilePath, 'utf8');
 
 const options = {
     ast: true,
-    configFile: '/Users/lishuxue/Documents/study/babel-transform/babel.config.ts.js',
-    filename: 'typescript.ts'
+    configFile: '/Users/lishuxue/Documents/study/babel-transform/babel.config.es6.js',
+    filename: 'es6.js'
 }
 const { ast, code } = babel.transformSync(esCode, options);
 
-console.log(ast);
-console.log(code);
+traverse(ast, {
+    // 遍历所有的导入语句
+    ImportDeclaration(path) {
+        console.log(path.node.source.value);
+    },
+    // 遍历所有的变量标识符
+    Identifier(path) {
+      console.log(path.node.name);
+    },
+});
